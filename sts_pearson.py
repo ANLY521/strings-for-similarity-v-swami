f1ec019b58f2d54d1af0fcbde65d6c00495397ef
v_swami <swamin.venkat2@gmail.com>
2022-02-16 23:40:24 -0500

    Test

--- sts_pearson.py
+++ sts_pearson.py
@@ -1,7 +1,14 @@
 from scipy.stats import pearsonr
 import argparse
 from util import parse_sts
+from nltk.translate.nist_score import sentence_nist
+from nltk import edit_distance, word_tokenize
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from difflib import SequenceMatcher
+import warnings
 
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 def main(sts_data):
     """Calculate pearson correlation between semantic similarity scores and string similarity metrics.
@@ -16,10 +23,48 @@ def main(sts_data):
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
 
+    #looking at the sts_nist.py code
+
+    nist_metric, bleu_metric, wer_metric, lcs_metric, ed_metric = [],[],[],[],[]
+
+    for words in texts:
+        t1, t2 = words
+        # input tokenized text
+        t1_toks = word_tokenize(t1.lower())
+        t2_toks = word_tokenize(t2.lower())
+
+        #Calculating NIST metric
+        try:
+            nist_metric = sentence_nist([t1_toks, ], t2_toks)
+        except ZeroDivisionError:
+            # print(f"\n\n\nno NIST, {i}")
+            nist_metric = 0.0
+
+        #Calculating bleu metrics
+        try:
+            bleu_metric = sentence_bleu([t1_toks, ], t2_toks, smoothing_function=SmoothingFunction().method0)
+        except ZeroDivisionError:
+            bleu_metric = 0.0
+            bleu_metric.append(bleu_score)
+
+        #Calculating WER metric
+        wer_val = edit_distance(t1_toks, t2_toks)/(len(t1_toks)+len(t2_toks))
+        wer_metric.append(wer_val)
+
+        #Calculating LCS metric
+        lcs_val = SequenceMatcher(None, t1, t2).ratio()
+        lcs_metric.append(lcs_val)
+
+        #Calculating ED metric
+        ed_val = edit_distance(t1, t2)
+        ed_metric.append(ed_val)
+
+
     #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
+
     for metric_name in score_types:
         score = 0.0
         print(f"{metric_name} correlation: {score:.03f}")
