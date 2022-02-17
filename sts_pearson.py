b60794bf8dc2721862002ef8095800a363799f67
Swami Venkat <45337354+v-swami@users.noreply.github.com>
2022-02-17 00:53:26 -0500

    Add files via upload

--- sts_pearson.py
+++ sts_pearson.py
@@ -34,46 +34,59 @@ def main(sts_data):
         t2_toks = word_tokenize(t2.lower())
 
         #Calculating NIST metric
+        #https://www.nltk.org/_modules/nltk/translate/nist_score.html
         try:
-            nist_metric = sentence_nist([t1_toks, ], t2_toks)
+            nist_val = sentence_nist([t1_toks, ], t2_toks)
         except ZeroDivisionError:
             # print(f"\n\n\nno NIST, {i}")
-            nist_metric = 0.0
+            nist_val = 0.0
+        nist_metric.append(nist_val)
 
         #Calculating bleu metrics
+        #https://www.askpython.com/python/bleu-score
         try:
-            bleu_metric = sentence_bleu([t1_toks, ], t2_toks, smoothing_function=SmoothingFunction().method0)
+            bleu_val = sentence_bleu([t1_toks, ], t2_toks, smoothing_function=SmoothingFunction().method0)
         except ZeroDivisionError:
-            bleu_metric = 0.0
-            bleu_metric.append(bleu_score)
+            bleu_val = 0.0
+        bleu_metric.append(bleu_val)
 
         #Calculating WER metric
+        #https://www.programcreek.com/python/?CodeExample=calculate+wer
         wer_val = edit_distance(t1_toks, t2_toks)/(len(t1_toks)+len(t2_toks))
         wer_metric.append(wer_val)
 
         #Calculating LCS metric
+        #https://stackoverflow.com/questions/48651891/longest-common-subsequence-in-python
         lcs_val = SequenceMatcher(None, t1, t2).ratio()
         lcs_metric.append(lcs_val)
 
         #Calculating ED metric
+        #https://stackoverflow.com/questions/2460177/edit-distance-in-python
         ed_val = edit_distance(t1, t2)
         ed_metric.append(ed_val)
-
 
     #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
 
-    for metric_name in score_types:
-        score = 0.0
-        print(f"{metric_name} correlation: {score:.03f}")
+    all_metrics = []
+    all_metrics.append(nist_metric)
+    all_metrics.append(bleu_metric)
+    all_metrics.append(wer_metric)
+    all_metrics.append(lcs_metric)
+    all_metrics.append(ed_metric)
+
+    for val in range(0,len(score_types)):
+        score = pearsonr(all_metrics[val],labels)[0]
+        print(f"{score_types[val]} correlation: {score:.03f}")
+
 
     # TODO 4: Complete writeup as specified by TODOs in README (describe metrics; show usage)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sts_data", type=str, default="stsbenchmark/sts-dev.csv",
+    parser.add_argument("--sts_data", type=str, default="stsbenchmark/sts-train.csv",
                         help="tab separated sts data in benchmark format")
     args = parser.parse_args()
 
